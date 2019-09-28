1cb2f68daca7a9e15f4f6b1d00d8f51edc0138ec
Patrick Rios <patrick.33.rios@hotmail.com>
2019-09-27 21:40:57 -0300

    Ajuste no item de lista cartão, botão de favoritos e marcador adicionados

--- src/controller/ListItemGridController.java
+++ src/controller/ListItemGridController.java
@@ -1,7 +1,11 @@
 package controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.CheckBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import model.bean.Persistent;
 import model.bean.Register;
@@ -18,6 +22,11 @@ public class ListItemGridController {
     private Label labelPaid;
     @FXML
     private Label labelExpiration;
+    @FXML
+    private Button buttonFavorite;
+    @FXML
+    private CheckBox checkboxItem;
+    private boolean favoriteControl = false;
     private Register register;
     private StackPane stack;
     
@@ -30,5 +39,29 @@ public class ListItemGridController {
     	this.labelPaid.setText(this.register.getPaidStatus());
     	this.labelExpiration.setText((this.register.isPaid())?"não":"sim");
     	this.labelValue.setText(this.register.getValueFormatted());
+    }
+    
+    @FXML
+    public void markAsFavorite()
+    {
+        Image icon = null;
+
+        if(favoriteControl){
+            this.favoriteControl = false;
+            icon = new Image(getClass().getResourceAsStream("/view/img/list/star-favorite-unselected.png"));
+        }
+
+        else{
+            this.favoriteControl = true;
+            icon = new Image(getClass().getResourceAsStream("/view/img/list/star-favorite-selected.png"));
+        }
+
+        this.buttonFavorite.setGraphic(new ImageView(icon));
+    }
+
+    @FXML
+    public void checkboxSelected()
+    {
+       
     }
 }
