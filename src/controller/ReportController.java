8d4ab6e164fdc85daaa0e7d322506a160d4f9729
Patrick Rios <patrick.33.rios@hotmail.com>
2019-11-17 02:22:34 -0300

    #report month picker added on report layout/controller

--- src/controller/ReportController.java
+++ src/controller/ReportController.java
@@ -1,9 +1,14 @@
 package controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import view.monthpicker.MonthPicker;
 
-public class ReportController {
+public class ReportController implements Initializable{
     @FXML
     private Label labelGenQuant;
     @FXML
@@ -16,4 +21,15 @@ public class ReportController {
     private Label labelExpenseTotal;
     @FXML
     private Label labelRevenueTotal;
+    @FXML
+    private Pane paneMonthFilter;
+    
+    private MonthPicker monthPicker;
+    
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		this.monthPicker = new MonthPicker();
+		this.paneMonthFilter.getChildren().add(this.monthPicker);
+		
+	}
 }
