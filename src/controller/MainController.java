7c7136c1e7ff1783bb81306aa06428c50418f4b9
Patrick Rios <patrick.33.rios@hotmail.com>
2019-11-19 16:46:11 -0300

    #Main main stack pane created and added

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -3,27 +3,30 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable
 {
+	@FXML
+    private StackPane stackMainContent;
     @FXML
     private AnchorPane anchorpaneContent;
     @FXML
     private AnchorPane menuConteiner;
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle)
-    {
+    public void initialize(URL url, ResourceBundle resourceBundle){
         loadMenu();
+        initStackAligment();
     }
 
-    private void loadMenu()
-    {
+    private void loadMenu(){
         FXMLLoader loader  = new FXMLLoader(getClass().getResource("/view/fxml/FXMLMenu.fxml"));
 
         try {
@@ -34,6 +37,10 @@ public class MainController implements Initializable
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+    
+    private void initStackAligment() {
+    	this.stackMainContent.setAlignment(Pos.TOP_LEFT);
     }
 
 }
