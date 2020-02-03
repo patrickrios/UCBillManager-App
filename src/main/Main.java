877d69b148c36f8dd7ff8a07b7672a88992e4eff
Patrick Rios <patrick.33.rios@hotmail.com>
2020-02-03 14:48:45 -0300

    #Main rebuilding main menu and logo (layout)

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,19 +8,16 @@ import javafx.stage.Stage;
 
 public class Main extends Application
 {
-
     @Override
-    public void start(Stage primaryStage) throws Exception
-    {
+    public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/view/fxml/FXMLMain.fxml"));
         primaryStage.setTitle("UC BillManager - Controle Financeiro");
-        primaryStage.setScene(new Scene(root, 500, 500));
+        primaryStage.setScene(new Scene(root));
         primaryStage.setMaximized(true);
         primaryStage.show();
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args){
         launch(args);
     }
 }
